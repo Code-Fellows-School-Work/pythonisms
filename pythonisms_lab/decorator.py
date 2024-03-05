26f3be68ba79a83414005fe6aa7dd4c3fcc6b746
evidad <errolvidad@gmail.com>
2024-03-05 15:26:53 -0800

    added debugging tool

--- pythonisms_lab/decorator.py
+++ pythonisms_lab/decorator.py
@@ -1,17 +1,29 @@
 from functools import wraps
 from time import sleep
 
+
 def delay(func):
     "Add time delay to rendered text"
     def wrapper(*args, **kwargs):
         sleep(3)
         return func(*args, **kwargs)
-    
+
     return wrapper
 
-@delay
+
+def debug(func):
+    def wrapper(*args, **kwargs):
+        problem = func(*args, **kwargs)
+        return "This is the problem " + problem
+
+    return wrapper
+
+
+# @delay
+@debug
 def example(text):
     return text
-    
+
+
 if __name__ == "__main__":
     print(example("Test Decorators"))
